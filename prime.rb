bf63a7618e15ed4e37dc7ad40c7bcd496f29a068
Kara O <karajo.odle@gmail.com>
2019-05-13 19:03:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+ def isPrime(num)
+      if num <= 1 
+        return false
+      end
+      
+      else if num <= 3
+        return true
+      end
+      
+      else if num % 2 == 0 || num % 3 == 0
+        return false
+      end
+      
+      i = 5;
+      while (i * i <= num) 
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+        i += 6;
+      }
+      return true
+    }
